670b4d7363ba08a877306fb1962a48fcff1f2ee1
Victor Pieretti <victor.pieretti@terra.com.br>
2024-04-23 00:01:09 -0300

    scripts.js

--- Previsão do Tempo/scripts.js
+++ Previsão do Tempo/scripts.js
@@ -1,20 +1,22 @@
 let chave = 'cebcd482eda57fa9a6714c1c2ba91885'
 
-function colocarNaTela(dados) {
+unction colocarNaTela(dados) {
     console.log(dados)
-
-    document.querySelector('.cidade').innerHTML = 'Tempo em ' + dados.name
-    document.querySelector('.temp').innerHTML = Math.floor(dados.main.temp) + '°C'
+    document.querySelector('.cidade').innerHTML = "Tempo em " + dados.name
+    document.querySelector('.temp').innerHTML = Math.floor(dados.main.temp) + "°C"
     document.querySelector('.icone').src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
     document.querySelector('.umidade').innerHTML = dados.main.humidity + "%"
+    document.querySelector('.weather-forecast').innerHTML = dados.weather[0].description
+    document.querySelector('.temp-2').innerHTML = Math.floor(dados.main.temp_max) + "°C máxima"
+    document.querySelector('.temp-2').innerHTML = Math.floor(dados.main.temp_min) + "°C mínima"
 }
 
 async function buscarCidade(cidade) {
-    let dados = await fetch(
-        'https://api.openweathermap.org/data/2.5/weather?q=' +
+    const dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
         cidade +
-        '&appid=cebcd482eda57fa9a6714c1c2ba91885&units=metric',
-    ).then((resposta) => resposta.json())
+        '&appid=cebcd482eda57fa9a6714c1c2ba91885&lang=pt_br&units=metric&units=metric').then(resposta => resposta.json())
+
+    colocarNaTela(dados)
 
     // AWAIT = ESPERE
     // FETCH -> Ferramenta do JavaScript para acessar servidores
